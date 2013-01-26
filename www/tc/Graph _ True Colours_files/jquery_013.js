5fc884b3e2ee6817ffdc40d2fc95775b3200b7fd
Tim Pizey <timp@paneris.org>
2013-01-26 17:45:47 +0000

    True colours files

---
+++ www/tc/Graph _ True Colours_files/jquery_013.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    /********************************************************************************************************************
+    ACCORDION
+    ********************************************************************************************************************/
+    $('.accordionButton').click(function () {
+        $('.accordionButton').removeClass('on');
+        $('.accordionContent').slideUp('normal');
+        if ($(this).next().is(':hidden') == true) {
+            $(this).addClass('on');
+            $(this).next().slideDown('normal');
+        }
+    });
+    $('.accordionButton').mouseover(function () {
+        $(this).addClass('over');
+    }).mouseout(function () {
+        $(this).removeClass('over');
+    });
+    $('.accordionContent').hide();
+
+
+
+});
